af6af86ba73263a953e646330537e6f376da283f
Luke Staudacher <lskhan@xemaps.com>
2020-03-01 09:07:26 -0700

    Update RightAdvancedAuto.java

--- Chronostar/src/main/java/frc/robot/commands/autos/RightAdvancedAuto.java
+++ Chronostar/src/main/java/frc/robot/commands/autos/RightAdvancedAuto.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.controls.AutoFiringSequence;
 import frc.robot.commands.controls.FireSequence;
+import frc.robot.commands.controls.SetFlyWheelVelocity;
+import frc.robot.commands.controls.SetHoodPosition;
 import frc.robot.commands.controls.SetIntakeSpeed;
 import frc.robot.commands.controls.TrackVisionTarget;
 import frc.robot.tools.controlLoops.PurePursuitController;
@@ -26,6 +28,6 @@ public class RightAdvancedAuto extends SequentialCommandGroup {
   public RightAdvancedAuto() {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super(new TrackVisionTarget(), new FireSequence(4500, 10, 1.0), new PurePursuitController(PathList.rightAutoPath1, 2.5, 4.0,true, true ),new PurePursuitController(PathList.rightAutoPath2, 2.5, 4.0,true, true ), new TrackVisionTarget(), new FireSequence(5500, 13.5, 1.0));
+    super(new TrackVisionTarget(), new FireSequence(4500, 10, 1.0), new PurePursuitController(PathList.rightAutoPath1, 2.5, 4.0,true, true ),new ParallelCommandGroup( new SetFlyWheelVelocity(5500), new SetHoodPosition(13.5),new PurePursuitController(PathList.rightAutoPath2, 2.5, 4.0,true, true )), new TrackVisionTarget(), new FireSequence(5500, 13.5, 1.8));
   }
 }
