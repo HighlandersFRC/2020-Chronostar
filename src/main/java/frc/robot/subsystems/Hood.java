299ba769860f01592bb8785a03e40bc9501bcae5
CalvinBlunk <javamaster097@gmail.com>
2020-10-23 15:06:58 -0600

    Pre-approval commit for the hood PID

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -2,27 +2,44 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.EncoderType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.RobotMap;
+import frc.robot.tools.controlloops.PID;
 
 public class Hood extends SubsystemBase {
+
+    double encValue;
+    double kP = 0.15;
+    double kI = 0.00015;
+    double kD = 0;
+
     public Hood() {}
 
-    double power = 0.05;
+    PID hoodPosition;
 
     public void init() {
         RobotMap.hoodMotor.setInverted(true);
+        hoodPosition = new PID(kP, kI, kD);
+        hoodPosition.setSetPoint(10);
+        hoodPosition.setMaxOutput(0.2);
+        hoodPosition.setMinOutput(-0.2);
     }
 
     @Override
     public void periodic() {
-
-        if (RobotMap.upperHoodSwitch.get()) {
-            power = -0.05;
-        } else if (RobotMap.lowerHoodSwitch.get()) {
-            power = 0.05;
+        encValue = RobotMap.hoodMotor.getEncoder(EncoderType.kHallSensor, 42).getPosition();
+        if (RobotMap.lowerHoodSwitch.get()) {
+            encValue = 0;
+        } else if (RobotMap.upperHoodSwitch.get()) {
+            encValue = 21;
         }
-        RobotMap.hoodMotor.set(power);
+        hoodPosition.updatePID(encValue);
+        RobotMap.hoodMotor.set(hoodPosition.getResult());
+        SmartDashboard.putNumber("enc value", encValue);
+        SmartDashboard.putNumber("hood power", hoodPosition.getResult());
     }
 }
