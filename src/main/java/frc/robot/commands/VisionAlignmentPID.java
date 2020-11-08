8d0e696da7d2db4d0867d96dea8d3796a58a51fc
CalvinBlunk <javamaster097@gmail.com>
2020-11-08 14:24:30 -0700

    Tuned up the vision alignment PID, ready for code review, merging, etc.

--- src/main/java/frc/robot/commands/VisionAlignmentPID.java
+++ src/main/java/frc/robot/commands/VisionAlignmentPID.java
@@ -4,79 +4,77 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
 import frc.robot.OI;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.tools.controlloops.PID;
 
 public class VisionAlignmentPID extends CommandBase {
-  /**
-   * Creates a new VisionAlignmentPID.
-   */
+    /** Creates a new VisionAlignmentPID. */
+    public VisionAlignmentPID() {
+        // Use addRequirements() here to declare subsystem dependencies.
+    }
 
-  public VisionAlignmentPID() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+    public static PID pid;
+    private final double kP = 0.01;
+    private final double kI = 0.00006;
+    private final double kD = 0.02;
 
-  public static PID pid;
-  private final double kP = 0.0085;
-  private final double kI = 0.00005;
-  private final double kD = 0.02;
+    @Override
+    public void initialize() {
+        pid = new PID(kP, kI, kD);
+        pid.setSetPoint(0);
+        pid.setMinOutput(-0.75);
+        pid.setMaxOutput(0.75);
+        RobotMap.visionRelay.set(Value.kForward);
+    }
 
-  @Override
-  public void initialize() {
-    pid = new PID(kP, kI, kD);
-    pid.setSetPoint(0);
-    pid.setMinOutput(-0.75);
-    pid.setMaxOutput(0.75);
-    RobotMap.visionRelay.set(Value.kForward);
-  }
+    public double jevoisAngle;
+    public double startingTime;
 
- public double jevoisAngle;
- public double startingTime;
-  @Override
-  public void execute() {
-    try{
-      startingTime = Timer.getFPGATimestamp();
-      Robot.visionCam.updateVision();
-      jevoisAngle = Robot.visionCam.getAngle();
-      SmartDashboard.putNumber("Jevois Angle", jevoisAngle);
-      SmartDashboard.putNumber("Get result", pid.getResult());
-      if(Timer.getFPGATimestamp() - Robot.visionCam.lastParseTime < 0.25){
-        pid.updatePID(jevoisAngle + 9.5);
-        RobotMap.leftDriveLead.set(ControlMode.PercentOutput, pid.getResult());
-        RobotMap.rightDriveLead.set(ControlMode.PercentOutput, -pid.getResult());
-      }
-      else{
-        pid.updatePID(0);
+    @Override
+    public void execute() {
+        try {
+            startingTime = Timer.getFPGATimestamp();
+            Robot.visionCam.updateVision();
+            jevoisAngle = Robot.visionCam.getAngle();
+            SmartDashboard.putNumber("Jevois Angle", jevoisAngle);
+            SmartDashboard.putNumber("Get result", pid.getResult());
+            if (Timer.getFPGATimestamp() - Robot.visionCam.lastParseTime < 0.25) {
+                pid.updatePID(jevoisAngle + 9.5);
+                RobotMap.leftDriveLead.set(ControlMode.PercentOutput, pid.getResult());
+                RobotMap.rightDriveLead.set(ControlMode.PercentOutput, -pid.getResult());
+            } else {
+                pid.updatePID(0);
+                RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
+                RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0);
+                RobotMap.visionRelay.set(Value.kForward);
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
         RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
         RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0);
-        RobotMap.visionRelay.set(Value.kForward);
-      }
-
-    } catch(Exception e) {
-  
+        RobotMap.visionRelay.set(Value.kReverse);
     }
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    RobotMap.leftDriveLead.set(ControlMode.PercentOutput, 0);
-    RobotMap.rightDriveLead.set(ControlMode.PercentOutput, 0);
-    RobotMap.visionRelay.set(Value.kReverse); 
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    if (!OI.driverController.getXButton()) {
-      return true;
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        if (!OI.driverController.getXButton()) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 }
