c1d4b6d4fcd172e71f1732f79cefb892e5cd4ebf
CalvinBlunk <javamaster097@gmail.com>
2020-12-02 17:39:08 -0700

    Changed around a few more things with the vision camera

--- src/main/java/frc/robot/sensors/VisionCamera.java
+++ src/main/java/frc/robot/sensors/VisionCamera.java
@@ -12,10 +12,6 @@ import org.json.simple.parser.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/* JSON From Camera :
-{"Distance": -11.0, "Angle": -100.0}
-*/
-
 public class VisionCamera {
     JSONParser parser = new JSONParser();
     SerialPort port;
@@ -23,8 +19,8 @@ public class VisionCamera {
     public double lastParseTime;
     private double distance;
     private double angle;
-    private double badAngle = -100.0;
-    private double badDistance = -11.0;
+    private static final double BAD_ANGLE = -100.0;
+    private static final double BAD_DISTANCE = -11.0;
     private Point targetPoint = new Point(0, 0);
     private AtomicBoolean shouldStop = new AtomicBoolean(false);
     private ConcurrentLinkedQueue<JSONObject> jsonResults = new ConcurrentLinkedQueue<JSONObject>();
@@ -35,9 +31,11 @@ public class VisionCamera {
                 () -> {
                     String buffer = "";
                     while (!shouldStop.get()) {
+                        // Gets bytes from serial port
                         if (port.getBytesReceived() > 0) {
                             buffer += port.readString();
                         }
+                        // Consume bytes until we see a '{'
                         if (buffer.length() > 0) {
                             int index = buffer.indexOf('{', 0);
                             if (index != -1 && index != 0) {
@@ -46,12 +44,16 @@ public class VisionCamera {
                                 buffer = "";
                             }
                         }
+                        // Search for '}' and parse JSON
                         if (buffer.length() > 0) {
                             int index = buffer.indexOf('}', 0);
                             if (index != -1) {
                                 String section = buffer.substring(0, index + 1);
                                 try {
                                     JSONObject json = (JSONObject) parser.parse(section);
+                                    if (jsonResults.size() > 16) {
+                                        jsonResults.poll();
+                                    }
                                     jsonResults.add(json);
                                 } catch (ParseException e) {
                                     System.err.println(e);
@@ -66,15 +68,25 @@ public class VisionCamera {
     }
 
     public void updateVision() {
+        // Drain queue
         JSONObject json = jsonResults.poll();
+        int currentSize = jsonResults.size();
+        for (int i = 0; i < currentSize; i++) {
+            JSONObject temp = jsonResults.poll();
+            if (temp != null) {
+                json = temp;
+            }
+        }
+
+        // Use JSON results if present
         if (json != null) {
             double tempDistance = (double) json.get("Distance");
             double tempAngle = (double) json.get("Angle");
 
-            if (tempDistance != badDistance) {
+            if (tempDistance != BAD_DISTANCE) {
                 distance = tempDistance;
             }
-            if (tempAngle != badAngle) {
+            if (tempAngle != BAD_ANGLE) {
                 angle = tempAngle;
             }
         }
