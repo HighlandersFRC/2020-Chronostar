e155286d64cc6e7bb60e4d1e1705bf7cc5ae43d2
CalvinBlunk <javamaster097@gmail.com>
2021-01-02 13:48:07 -0700

    Made changes to MagIntakeDefault percentages

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,39 +11,11 @@ import frc.robot.sensors.*;
 
 public class RobotMap {
 
-<<<<<<< HEAD
-    public static TalonFX intakeMotor = new TalonFX(Constants.INTAKE_1_ID);
-    public static VictorSPX intake2Motor = new VictorSPX(Constants.INTAKE_2_ID);
-    public static TalonFX leftFlywheel = new TalonFX(Constants.LEFT_FLYWHEEL_ID);
-    public static TalonFX rightFlywheel = new TalonFX(Constants.RIGHT_FLYWHEEL_ID);
-    public static VictorSPX lowMag = new VictorSPX(Constants.MAG_BELT_ID);
-    public static CANSparkMax highMag =
-            new CANSparkMax(Constants.MAG_WHEEL_ID, MotorType.kBrushless);
-    public static CANSparkMax hoodMotor = new CANSparkMax(Constants.HOOD_ID, MotorType.kBrushless);
-
-    public static DoubleSolenoid intakePiston = new DoubleSolenoid(0, 1);
-    public static DoubleSolenoid climberPiston = new DoubleSolenoid(2, 3);
-    public static DoubleSolenoid winchRelease = new DoubleSolenoid(4, 5);
-
-    public static DigitalInput beambreak1 = new DigitalInput(Constants.BEAM_BREAK_1_ID);
-    public static DigitalInput beambreak2 = new DigitalInput(Constants.BEAM_BREAK_3_ID);
-    public static DigitalInput beambreak3 = new DigitalInput(Constants.BEAM_BREAK_2_ID);
-
-    public static CANDigitalInput lowerHoodSwitch =
-            new CANDigitalInput(hoodMotor, LimitSwitch.kReverse, LimitSwitchPolarity.kNormallyOpen);
-    public static CANDigitalInput upperHoodSwitch =
-            new CANDigitalInput(hoodMotor, LimitSwitch.kForward, LimitSwitchPolarity.kNormallyOpen);
-
-    public static Drive drive = new Drive();
-    public static Magazine magazine = new Magazine();
-    public static Intake intake = new Intake();
-=======
     /*
      * TODO Om, you need to move your subsystems' hardware declarations into their
      * TODO own subsystems. You'll also have to create classes for your subsystems,
      * TODO and create default commands. Use Adi's and my code as a reference.
      */
->>>>>>> 8e0565b9ed40c562f07f411e066bf1934f0862e6
 
     public static AHRS ahrs = new AHRS(Port.kMXP);
     public static Navx navx = new Navx(ahrs);
@@ -52,8 +24,6 @@ public class RobotMap {
     public static LidarLite lidar = new LidarLite(lidarPort);
 
     public static Relay visionRelay = new Relay(0);
-<<<<<<< HEAD
-=======
 
     public static VisionCamera visionCam;
 
@@ -66,5 +36,4 @@ public class RobotMap {
         }
         visionCam = new VisionCamera(jevois);
     }
->>>>>>> 8e0565b9ed40c562f07f411e066bf1934f0862e6
 }
