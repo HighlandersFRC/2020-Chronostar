f1c8796f474eb74abca28593ae293318ca31d471
CalvinBlunk <javamaster097@gmail.com>
2021-01-13 17:53:40 -0700

    Modified Intake to not break

--- src/main/java/frc/robot/commands/basic/Intake.java
+++ src/main/java/frc/robot/commands/basic/Intake.java
@@ -1,5 +1,4 @@
 // Copyrights (c) 2018-2019 FIRST, 2020 Highlanders FRC. All Rights Reserved.
-
 package frc.robot.commands.basic;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,16 +7,20 @@ import frc.robot.OI;
 import frc.robot.subsystems.MagIntake;
 
 public class Intake extends CommandBase {
+    /** Creates a new Intake. */
     private MagIntake magIntake;
 
     public Intake(MagIntake magIntake) {
         this.magIntake = magIntake;
         addRequirements(magIntake);
+        // Use addRequirements() here to declare subsystem dependencies.
     }
 
+    // Called when the command is initially scheduled.
     @Override
     public void initialize() {}
 
+    // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         if (OI.operatorRB.get()) {
@@ -45,9 +48,11 @@ public class Intake extends CommandBase {
         }
     }
 
+    // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {}
 
+    // Returns true when the command should end.
     @Override
     public boolean isFinished() {
         if (!OI.operatorRB.get()) {
