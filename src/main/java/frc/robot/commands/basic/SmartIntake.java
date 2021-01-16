7fd07754c0b2d931a61dadc71487239dca7c1087
CalvinBlunk <javamaster097@gmail.com>
2021-01-15 18:20:59 -0700

    Added print statements to see where SmartIntake breaks

--- src/main/java/frc/robot/commands/basic/SmartIntake.java
+++ src/main/java/frc/robot/commands/basic/SmartIntake.java
@@ -40,27 +40,34 @@ public class SmartIntake extends CommandBase {
             if (highMagTimer > 0) {
                 magIntake.setHighMagPercent(HIGH_MAG_POWER);
                 highMagTimer -= LOOP_TIME;
+                System.out.println("starting high mag motor");
             } else {
                 magIntake.setHighMagPercent(0);
                 highMagTimer = 0;
+                System.out.println("stopping high mag motor");
             }
             if (lowMagTimer > 0) {
                 magIntake.setLowMagPercent(LOW_MAG_POWER);
                 lowMagTimer -= LOOP_TIME;
+                System.out.println("starting low mag motor");
             } else {
                 magIntake.setLowMagPercent(0);
                 lowMagTimer = 0;
+                System.out.println("stopping low mag motor");
             }
 
             // Checking beam breaks to initialize countdowns
             if (magIntake.getBeamBreak(BeamBreakID.ONE)) {
                 lowMagTimer = PULSE_DURATION;
+                System.out.println("setting low mag timer");
             }
             if (magIntake.getBeamBreak(BeamBreakID.THREE)) {
                 highMagTimer = 0;
+                System.out.println("stopping high mag timer");
             } else if (magIntake.getBeamBreak(BeamBreakID.TWO)) {
                 lowMagTimer = PULSE_DURATION;
                 highMagTimer = PULSE_DURATION;
+                System.out.println("setting low and high mag timer");
             }
         } else if (direction == IntakeDirection.OUT) {
             magIntake.setIntakePercent(-LOW_INTAKE_POWER, -HIGH_INTAKE_POWER);
@@ -69,7 +76,9 @@ public class SmartIntake extends CommandBase {
     }
 
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        System.out.println("ending");
+    }
 
     @Override
     public boolean isFinished() {
